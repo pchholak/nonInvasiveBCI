a936d7dc66f662e47cb69823bfa7ff1b487aaca2
pchholak <parthchholak@gmail.com>
2021-04-30 18:09:40 +0200

    added more comments until read_data

--- FrequencyAnal/main.m
+++ FrequencyAnal/main.m
@@ -1,19 +1,22 @@
-xxx
+close all
+clear
+clc
 
-iSubj = 1;
-info.channels = 7:8;
+iSubj = 1; % Subject number
+info.channels = 7:8; % EEG channels to analyse
 info.time_events = [61142.122, 61302.21, 61572.246, 61842.272];
 info.conds = {'B', 'F', 'F', 'F'};
 info.data_path = 'C:/Users/Usuario/data/BCI';
-info.trial_length = 240; % in seconds
-info.Fs = 250; % in Hz
-info.plot_PSD = 0;
+info.trial_length = 120; % in seconds
+info.Fs = 250; % Sampling frequency (Hz)
+info.plot_PSD = 0; % Flag to plot power spectral density or not
 t = (1 : info.Fs*info.trial_length) / info.Fs;
 
 sprintf('Loading and segmenting data into trials...')
 [trials, time, eeg] = read_data(iSubj, info);
 
 % Check power spectrum for tags
+sprintf('Computing power spectrum...')
 [pm_B, f] = findPSD(trials{1}(info.channels, :), info.Fs);
 n_trials = numel(info.time_events);
 pm = nan(numel(pm_B), 3);
--- FrequencyAnal/read_data.m
+++ FrequencyAnal/read_data.m
@@ -1,22 +1,25 @@
 function [trials, time, eeg] = read_data(iSubj, info)
 % Function to read data in text format to a mat variable
 
-subject_name = sprintf('sub%02d', iSubj);
-fname = fullfile(info.data_path, subject_name, 'RAW.txt');
+subject_name = sprintf('sub%02d', iSubj); % subject number to name string
+fname = fullfile(info.data_path, subject_name, 'RAW.txt'); % join strings to
+% get final path/location of the input file.
 
+fileID = fopen(fname); % Open file to get file ID
 formatSpec = ['%*s' repmat('%f', 1, 8) repmat('%*s', 1, 3) '%{HH:mm:ss.SSS}D'];
-fileID = fopen(fname);
 data = textscan(fileID, formatSpec, 'delimiter', ',', 'headerlines', 7, ...
         'collectoutput', 1);
 fclose(fileID);
 
-eeg = data{1}';
-timeObj = data{2};
-time = timeObj.Hour * 3600 + timeObj.Minute * 60 + timeObj.Second;
+eeg = data{1}'; % All EEG data from 8 channels and total recording time
+timeObj = data{2}; % Recording time stored as datetime object in MATLAB
+time = timeObj.Hour * 3600 + timeObj.Minute * 60 + timeObj.Second; % Recording
+% time converted to seconds in the day after 00:00 AM.
 
 % Find nearest timestamps to recording events
-t0 = info.time_events(1); T = floor(info.time_events - t0);
-err = abs(time-t0); [~, i_min] = min(err);
+t0 = info.time_events(1); % Time at which first event (background) started
+T = floor(info.time_events - t0); % Change time reference to first event
+err = abs(time-t0); [~, i_min] = min(err); % time <-> OpenBCI; t0 <-> MATLAB
 
 inx_events = i_min + T * info.Fs;
 
